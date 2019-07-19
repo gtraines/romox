7f1b5ff5403ea606ba03f89510751e284d026922
Graham Traines <gtraines@hotmail.com>
2019-07-18 18:38:57 -0700

    More reorg

---
+++ src/ServerScriptService/AppStart/FireTruckStart.server.lua
@@ -0,0 +1,8 @@
+
+
+local module = {
+
+}
+
+
+return module
--- src/ServerScriptService/GameControllers/VocationManagement/UniformMorphCommon.lua
+++
@@ -1,5 +0,0 @@
-
-function AttachEquipmentToCharacter(equipmentToAttach, playerObj)
-    print(equipmentToAttach, playerObj)
-    return 2
-end
