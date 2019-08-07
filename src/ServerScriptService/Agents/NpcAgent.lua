6e2cede2032c6d21cd0f0a0ecf7167aa5533cef1
Graham Traines <gtraines@hotmail.com>
2019-08-06 22:15:30 -0700

    Npc movement

--- src/ServerScriptService/Agents/NpcAgent.lua
+++ src/ServerScriptService/Agents/NpcAgent.lua
@@ -1,26 +1,40 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+local libFinder = require(ServerScriptService
+    :WaitForChild("Finders", 2)
+    :WaitForChild("LibFinder", 2))
+
+local uuid = libFinder:FindLib("uuid")
+
 local agent = {
-    _playerProto = {}
+    ManagedEntities = {}
 }
 
-local playerProto = {
-    Name = "",
-    Character = {
-        Humanoid = {}
-    }
-}
-
-local proto = Instance.new("Player")
-
-agent._playerProto = playerProto:Clone()
-
-function agent.new( name, humanoid )
-    -- body
-    local agentInstance = agent._playerProto:Clone()
-    agentInstance.Name = name
-    agentInstance.Character.Humanoid = humanoid:Clone()
-
-    return agentInstance
+function agent._getOrAddEntityIdToObj(object)
+    local entityIdObj = object:FindFirstChild("EntityId")
+    if entityIdObj == nil then
+        entityIdObj = Instance.new("StringValue")
+        entityIdObj.Name = "EntityId"
+        entityIdObj.Parent = createdItem
+    end
+    entityIdObj.Value = uuid()
+    return entityIdObj.Value
 end
 
+function agent._wrapPersonage(name, personage)
+    local nonPlayerModel = Instance.new("Model", game.Workspace)
+    
+    nonPlayerModel.Name = name
+    personage.Name = "Personage"
+    personage.Parent = nonPlayerModel
+    return nonPlayerModel
+end
+
+function agent.new( name, personage )
+    -- body
+    local nonPlayer = agent._wrapPersonage(name, personage)
+    local entityId = agent._getOrAddEntityIdToObj(nonPlayer)
+    agent.ManagedEntities[entityId] = nonPlayer
+    return nonPlayer
+end
 
 return agent
