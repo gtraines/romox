d2d20122585bc9ff15745f1c04f995c797a63fae
Graham Traines <gtraines@hotmail.com>
2019-08-07 22:45:50 -0700

    StateMachine rework

--- src/ServerScriptService/Agents/CatHerder.lua
+++ src/ServerScriptService/Agents/CatHerder.lua
@@ -1,1 +1,5 @@
 -- Orchestrate behavior of NPCs after creation
+-- Connect to CollectionService, monitor NPCs added to various collections
+ -- Categorize by State? (State.Name)
+ -- Something else?
+ 
