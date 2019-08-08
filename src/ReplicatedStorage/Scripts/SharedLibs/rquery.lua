0a5d8d9d7b21503c4764b573743e614b1d79a6ad
Graham Traines <gtraines@hotmail.com>
2019-08-08 09:38:06 -0700

    Added roact

--- src/ReplicatedStorage/Scripts/SharedLibs/rquery.lua
+++ src/ReplicatedStorage/Scripts/SharedLibs/rquery.lua
@@ -145,4 +145,16 @@ function module.GetOrAddItem( itemName, itemType, parent )
 	return itemCandidate
 end
 
+function module.Assign( target, ... )
+	for i = 1, select("#", ...) do
+		local source = select(i, ...)
+
+		for key, value in pairs(source) do
+			target[key] = value
+		end
+	end
+
+	return target
+end
+
 return module
